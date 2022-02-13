35e4075726364173b96c8095ea6974e15a6be88a
binarybrain11 <zatcat6@gmail.com>
2022-02-12 17:26:13 -0800

    Chapter 5 Homework implemented

--- C/cryptointeractive.c
+++ C/cryptointeractive.c
@@ -4,6 +4,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include "cryptointeractive.h"
 
@@ -235,6 +236,199 @@ double hw2_1OtsAdvantage(ssize_t lambda, unsigned int trials, char (*attack)(ssi
     double advantage = 0;
     for (unsigned int i=0; i<trials; i++){
         advantage += hw2_1OtsAttack(lambda, attack);
+    }
+    return advantage/(double) trials;
+}
+
+/* ==================================================================
+ * CHAPTER 5
+ * ==================================================================
+ */
+
+/* Chapter 5 Homework Problem 1 a */
+/* This is a *secure* length tripling PRG. stdlib rand() isn't cryptographically
+ * secure, however it can be seeded and we assume users won't attack rand() but
+ * rather the scheme described in the problem. The seed to srand() is limited to 
+ * sizeof(int) so input seeds larger than that will be truncated.
+ */
+/* Length tripling PRG */
+char* hw5_1G(ssize_t lambda, char* s){
+    char* num = malloc(3*lambda*sizeof(char));
+    /* Safely converts s to an integer to pass as seed to srand() */
+    int seed = 0;
+    for (int i=0; i<lambda && i < sizeof(int); i++){
+        seed += s[i];
+        seed << 8*sizeof(char);
+    }
+
+    srand(seed);
+    for (int i=0; i<3*lambda; i++){
+        num[i] = rand() & 0xFF;
+    }
+    return num;
+}
+
+char* hw5_1aPRGreal(ssize_t lambda){
+    char* s = randomBytes(lambda);
+    char* x = hw5_1G(lambda, s);
+    char* zero = zeroBytes(lambda);
+    char* y = hw5_1G(lambda, zero);
+    /* We return x||y where x and y are each 3*lambda bytes long */
+    char* res = malloc(6*lambda*sizeof(char));
+    for (int i=0; i<3*lambda; i++){
+        res[i] = x[i];
+    }
+    for (int i=3*lambda; i<6*lambda; i++){
+        res[i] = y[i];
+    }
+    free(s);
+    free(x);
+    free(y);
+    free(zero);
+    return res;
+}
+
+char* hw5_1aPRGrand(ssize_t lambda){
+    return randomBytes(lambda);
+}
+
+int hw5_1aPrgAttack(ssize_t lambda, char (*attack)(ssize_t, Scheme*)){
+    Scheme scheme = {NULL, NULL, NULL, NULL, NULL, NULL, NULL};
+    char* choice = randomBytes(1);
+    if (*choice & 2){
+        scheme.QUERY = hw5_1aPRGrand;
+    } else {
+        scheme.QUERY = hw5_1aPRGreal;
+    }
+    char result = attack(lambda, &scheme);
+    if ((*choice & 2) == 2 && result == '$'){
+        free(choice);
+        return 1;
+    } 
+
+    if ((*choice & 2) == 0 && result == 'r'){
+        free(choice);
+        return 1;
+    } 
+    
+    free(choice);
+    return 0;
+}
+
+double hw5_1aPrgAdvantage(ssize_t lambda, unsigned int trials, char (*attack)(ssize_t, Scheme*)){
+    double advantage = 0;
+    for (unsigned int i=0; i<trials; i++){
+        advantage += hw5_1aPrgAttack(lambda, attack);
+    }
+    return advantage/(double) trials;
+}
+
+/* Chapter 5 Homework Problem 1b */
+char* hw5_1bPRGreal(ssize_t lambda){
+    char* s = randomBytes(lambda);
+    char* x = hw5_1G(lambda, s);
+    char* zero = zeroBytes(lambda);
+    char* y = hw5_1G(lambda, zero);
+    /* x = x^y then return the new x */
+    for (int i=0; i<3*lambda; i++){
+        x[i] ^= y[i];
+    }
+    free(s);
+    free(y);
+    free(zero);
+    return x;
+}
+
+char* hw5_1bPRGrand(ssize_t lambda){
+    return randomBytes(lambda);
+}
+
+int hw5_1bPrgAttack(ssize_t lambda, char (*attack)(ssize_t, Scheme*)){
+    Scheme scheme = {NULL, NULL, NULL, NULL, NULL, NULL, NULL};
+    char* choice = randomBytes(1);
+    if (*choice & 2){
+        scheme.QUERY = hw5_1bPRGrand;
+    } else {
+        scheme.QUERY = hw5_1bPRGreal;
+    }
+    char result = attack(lambda, &scheme);
+    if ((*choice & 2) == 2 && result == '$'){
+        free(choice);
+        return 1;
+    } 
+
+    if ((*choice & 2) == 0 && result == 'r'){
+        free(choice);
+        return 1;
+    } 
+    
+    free(choice);
+    return 0;
+}
+
+double hw5_1bPrgAdvantage(ssize_t lambda, unsigned int trials, char (*attack)(ssize_t, Scheme*)){
+    double advantage = 0;
+    for (unsigned int i=0; i<trials; i++){
+        advantage += hw5_1bPrgAttack(lambda, attack);
+    }
+    return advantage/(double) trials;
+}
+
+/* Chapter 5 Homework Problem 1c */
+char* hw5_1cPRGreal(ssize_t lambda){
+    char* s = randomBytes(lambda);
+    /* xyz = x||y||z = G(s) */
+    char* xyz = hw5_1G(lambda, s);
+    /* Even though xyz has 3*lambda length, the function will only read the
+     * first lambda bytes, or x, for the seed
+     */
+    char* w = hw5_1G(lambda, xyz);
+    /* res = x||y||z||w */
+    char* res = malloc(6*lambda*sizeof(char));
+    for (int i=0; i<3*lambda; i++){
+        res[i] = xyz[i];
+    }
+    for (int i=3*lambda; i<6*lambda; i++){
+        res[i] = w[i];
+    }
+
+    free(s);
+    free(xyz);
+    free(w);
+    return res;
+}
+
+char* hw5_1cPRGrand(ssize_t lambda){
+    return randomBytes(lambda);
+}
+
+int hw5_1cPrgAttack(ssize_t lambda, char (*attack)(ssize_t, Scheme*)){
+    Scheme scheme = {NULL, NULL, NULL, NULL, NULL, NULL, NULL};
+    char* choice = randomBytes(1);
+    if (*choice & 2){
+        scheme.QUERY = hw5_1cPRGrand;
+    } else {
+        scheme.QUERY = hw5_1cPRGreal;
+    }
+    char result = attack(lambda, &scheme);
+    if ((*choice & 2) == 2 && result == '$'){
+        free(choice);
+        return 1;
+    } 
+
+    if ((*choice & 2) == 0 && result == 'r'){
+        free(choice);
+        return 1;
+    } 
+    
+    free(choice);
+    return 0;
+}
+
+double hw5_1cPrgAdvantage(ssize_t lambda, unsigned int trials, char (*attack)(ssize_t, Scheme*)){
+    double advantage = 0;
+    for (unsigned int i=0; i<trials; i++){
+        advantage += hw5_1cPrgAttack(lambda, attack);
     }
     return advantage/(double) trials;
 }
--- C/cryptointeractive.h
+++ C/cryptointeractive.h
@@ -33,3 +33,22 @@ double se2_3OtsAdvantage(ssize_t lambda, unsigned int trials, char (*attack)(ssi
 /* Implements CTXT() and EAVESDROP() */
 int hw2_1OtsAttack(ssize_t lambda, char (*attack)(ssize_t, Scheme*));
 double hw2_1OtsAdvantage(ssize_t lambda, unsigned int trials, char (*attack)(ssize_t, Scheme*));
+
+/* Chapter 5 Homework Pronlem 1 */
+/* *secure* length tripling PRG. Not actually secure, but treat it as such. 
+ * Seeds given to this function are truncated to sizeof(int) so consider using 
+ * a size less than or equal to sizeof(int) for lambda 
+ */
+char* hw5_1G(ssize_t lambda, char* s);
+/* Chapter 5 Homework Pronlem 1a */
+/* Implements QUERY() */
+int hw5_1aPrgAttack(ssize_t lambda, char (*attack)(ssize_t, Scheme*));
+double hw5_1aPrgAdvantage(ssize_t lambda, unsigned int trials, char (*attack)(ssize_t, Scheme*));
+/* Chapter 5 Homework Pronlem 1b */
+/* Implements QUERY() */
+int hw5_1bPrgAttack(ssize_t lambda, char (*attack)(ssize_t, Scheme*));
+double hw5_1bPrgAdvantage(ssize_t lambda, unsigned int trials, char (*attack)(ssize_t, Scheme*));
+/* Chapter 5 Homework Pronlem 1c */
+/* Implements QUERY() */
+int hw5_1cPrgAttack(ssize_t lambda, char (*attack)(ssize_t, Scheme*));
+double hw5_1cPrgAdvantage(ssize_t lambda, unsigned int trials, char (*attack)(ssize_t, Scheme*));
